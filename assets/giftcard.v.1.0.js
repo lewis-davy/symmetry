e7c8192973fc168db9deb875676f47c84060ed70
Lewis Davy <lewis.davy@strawberry.co.uk>
2022-10-27 15:32:34 +0100

    Update to v5.5.1

--- assets/giftcard.v.1.0.js
+++ assets/giftcard.v.1.0.js
@@ -1,1 +1,47 @@
-!function(){var e="print-gift-card",t="gift-card-code-digits",n=document.getElementById("qr-code");new QRCode(n,{text:n.getAttribute("data-identifier"),width:100,height:100}),document.getElementById(e).addEventListener("click",function(){window.print()},!1),document.getElementById(t).addEventListener("click",function(e){var t=e.target,n="";document.body.createTextRange?((n=document.body.createTextRange()).moveToElementText(t),n.select()):window.getSelection&&(e=window.getSelection(),(n=document.createRange()).selectNodeContents(t),e.removeAllRanges(),e.addRange(n))},!1)}();
+/**
+ * v.1.0
+ *
+ * Gift Card Template Script
+ * ------------------------------------------------------------------------------
+ * A file that contains scripts highly couple code to the Gift Card template.
+ */
+
+(function () {
+  var config = {
+    qrCode: 'qr-code',
+    printButton: 'print-gift-card',
+    giftCardCode: 'gift-card-code-digits' };
+
+
+  var qrCode = document.getElementById(config.qrCode);
+
+  new QRCode(qrCode, {
+    text: qrCode.getAttribute('data-identifier'),
+    width: 120,
+    height: 120 });
+
+
+  document.getElementById(config.printButton).addEventListener('click', function () {
+    window.print();
+  }, false);
+
+  function selectText(evt) {
+    var text = evt.target;
+    var range = '';
+
+    if (document.body.createTextRange) {
+      range = document.body.createTextRange();
+      range.moveToElementText(text);
+      range.select();
+    } else if (window.getSelection) {
+      var selection = window.getSelection();
+      range = document.createRange();
+      range.selectNodeContents(text);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+  }
+
+  // Auto-select gift card code on click, based on ID passed to the function
+  document.getElementById(config.giftCardCode).addEventListener('click', selectText, false);
+})();
